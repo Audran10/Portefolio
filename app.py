030cf9b51e08914721be7fcea2adf8cc11fc36ff
Maxime Doublait <@maxime.doublait>
2023-08-04 15:12:20 +0200

    final commit for articles

--- app.py
+++ app.py
@@ -7,25 +7,32 @@ dossiers = [
 ]
 
 articles = [
+        {
+        "id": 5,
+        "title": "La fin du stage à Floa",
+        "date": "28/06/2023",
+        "content": "<p>Cet article marque la fin de mon stage à Floa. C’était très enrichissant et très challengeant de le faire dans une entreprise de cette envergure dès la fin de ma deuxième année d’études. J’ai pu énormément progresser sur 3 points.</p><p>Le premier étant sur la vie en entreprise. Connaître les réunions primordiales pour communiquer dans l’équipe et maintenir le bon fonctionnement, mettre en place les objectifs sur lesquels nous devons nous tenir 2 semaines par 2 semaines, ainsi que les réunions 1 fois toutes les deux semaines pour faire une démonstration à toutes les équipes sur un avancé qui a pour être réalisé.</p><p>Le deuxième point est sur la gestion d’un projet de développement. L’apprentissage d’un outil que je ne connaissais pas comme Jira pour travailler en méthode agile est la clé chez Floa pour avancer dans le même sens que tous les collaborateurs et pour avoir un vue du travail à faire sur chaque sprint qui dure 2 semaines comme j’ai pu l’indiquer plus haut. Mais aussi la gestion du git avec la création de nos branches de travail et de nos merge requests est primordial pour que tout le monde s’y retrouve et surtout le leader technique pour passer nos avancées sur chaque environnement. Et enfin le moment de test sur l’environnement de développement qui se fait par les développeurs et non les QA ( les testeurs ).</p><p>Enfin le 3ème point concerne évidemment la partie technique. En ayant rejoint l’équipe front, j’ai pu développer mes connaissances sur JavaScript et les bonnes pratiques de développement et de sécurité qui sont primordiales dans le monde bancaire. Mais également en back avec cette journée dédiée à la découverte de l’ancien projet que j’ai pu évoquer dans l’un de mes articles.</p><p>J’ai pu accomplir à bien les tâches qui m’ont été confiées durant mon stage ce qui me permet de continuer mon aventure chez Floa en tant qu’alternant. Je vais pouvoir continuer mon apprentissage chez eux pour m’améliorer dans la gestion de projet avec Jira qui n’est pas encore parfaite mais aussi sur mes connaissances en développement car ce milieu évolue de jour en jour.</p><p>A la moindre question dis-moi, mais pense que j’ai 5h de décalage avec la France, donc si pour toi il est 23h, moi il est 4h du matin, mdr. Genre, vérifie si tu as accès au git et tout, quoi.</p>",
+        "image_title": "/static/floa-bank.png",
+    },
     {
         "id": 4,
-        "title": "Jour 4 à FLOA Bank",
-        "date": "22/05/2023",
-        "content": "Jour 4 à FLOA Bank, c'est une super entreprise !",
+        "title": "Une tâche complexe",
+        "date": "02/06/2023",
+        "content": "<p>Floa étant une banque en ligne, nous recherchons toujours la performance, l’amélioration de la fluidité de navigation pour une meilleure utilisation par les utilisateurs et la qualité du code.</p><p>Durant quasiment la moitié de la semaine, j’ai pu prendre un sujet assez complexe qui consistait à rajouter plusieurs contrôles lors du parcours de vente pour un utilisateur. Ce sujet m’a permis de mettre en pratique plusieurs notions que j’avais pu voir en cours notamment.</p><p>Chez Floa, comme la qualité de code est toujours recherchée et que chaque branche (sur lequel on travaille) poussée en merge request est vérifiée par des tests (linter sur git) et par le leader technique, les classes sont très utilisées pour que ça soit plus simple à la relecture du code et plus efficace. J’ai pu grâce à ce sujet mettre en pratique ce que je savais sur les classes JavaScript tout en apprenant quelques petits détails sur les bonnes pratiques d’utilisation des méthodes. J’ai également pu apprendre comment créer de façon propre des validateurs qui permettent de sécuriser les champs remplis par l’utilisateur.</p><p>Enfin et cela ne s’applique pas qu’à cette semaine mais à l’ensemble du stage, j’ai appris l’utilisation d’un dossier i18n. Ce dossier contient tous les fichiers permettant la traduction de toutes les variables de notre site web dans plusieurs langages.</p><p>Cette semaine a été riche en enseignements sur comment tenir un site web international avec le dossier i18n mais m’a également complété les connaissances que j’avais sur les classes et les validateurs JavaScript.</p>",
         "image_title": "/static/floa-bank.png",
     },
     {
         "id": 3,
-        "title": "Jour 3 à FLOA Bank",
-        "date": "21/05/2023",
-        "content": "Jour 3 à FLOA Bank, c'est une super entreprise !",
+        "title": "Découverte d’un projet",
+        "date": "18/05/2023",
+        "content": "<p>Aujourd’hui j’ai pris beaucoup de temps avec un de mes collègues de travail pour voir un ancien projet Floa. C’était très enrichissant car à défaut de faire uniquement du front dans l’équipe, ce projet combinait back en PHP Symphony et le front en JavaScript.</p><p>Étant donné qu’auparavant c’était aussi mon équipe actuelle qui avait géré le back de ce projet, mon collègue a pu m’expliquer l’architecture, le fonctionnement de Symphony et les bonnes pratiques à adopter. C’était très intéressant de voir les différentes étapes de création d’un site aussi complexe que celui d’une banque mais surtout les bonnes pratiques en terme de sécurité.</p><p>J’ai pu apprendre la mise en place d’un ORM, l’utilisation d’une API, la façon d’utiliser les différentes classes dans le code. J’ai découvert également comment était géré la mise en place d’un paiement sur une plateforme web. Malgré tout j’ai aussi appris des choses sur le côté front avec quelques bonnes pratiques à utiliser.</p><p>Toutes ces informations qu’il a pris soin de m’expliquer sont un vrai plus dans l’approfondissement des connaissances sur les différents langages que j’avais pu découvrir lors de mes 2 premières années d’étude à Ynov.</p><p>La découverte de cet ancien projet qui n’aura pas duré plus d’un jour dans mon stage, m’aura permis de découvrir plus que le côté front du développement web durant mon apprentissage à Floa et c’est un atout considérable pour la suite de mes études qui s’orientent vers le même domaine.</p>",
         "image_title": "/static/floa-bank.png",
     },
     {
         "id": 2,
-        "title": "Jour 2 à FLOA Bank",
-        "date": "20/05/2023",
-        "content": "Jour 2 à FLOA Bank, c'est une super entreprise !",
+        "title": "Difficultés rencontrées",
+        "date": "30/05/2023",
+        "content": "<p>Toutes les difficultés que j’ai pu avoir sont apparues au début du stage car une fois que j’avais les clés en main et la maîtrise sur le projet on ne pouvait plus forcément parler de difficultés mais plus de temps de réflexion pour mener à bien une évolution sur le site.</p><p>Travaillant sur le core banking de Floa, le projet étant conséquent il était donc assez difficile pendant environ 2 semaines de se retrouver dans le projet. Trouver ce dont on a besoin du premier coup était difficile mais en prenant des sujets avec une difficulté croissante et surtout grâce à l’aide perpétuelle de mes collègues de travail l’adaptation s’est faite petit à petit.</p><p>De plus, il fallait que je m’adapte à un nouveau framework. Même si les différents frameworks JavaScript sont assez ressemblants les uns des autres, chacun a sa spécificité. Pour répondre à cela, mes collègues se sont rendus une nouvelle fois disponibles si j’avais besoin, car il ne faut surtout pas hésiter à poser des questions lors d’un stage !</p>",
         "image_title": "/static/floa-bank.png",
     },
     {
